585ab6b160c09e3991f2a2cb8c6809b34497dc6c
Cleber Rosa <crosa@redhat.com>
2024-02-22 05:25:19 -0500

    Add Python code lint check
    This updates the avocado-static-check submodule, and implements the
    changes necessary to have the lint checks passing.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- autils/archive/ar.py
+++ autils/archive/ar.py
@@ -80,8 +80,8 @@ class Ar:
                 member = struct.unpack(
                     FILE_HEADER_FMT, open_file.read(FILE_HEADER_SIZE)
                 )
-            except struct.error:
-                raise StopIteration
+            except struct.error as exc:
+                raise StopIteration from exc
 
             # No support for extended file names
             identifier = member[0].decode("ascii").strip()
@@ -105,3 +105,4 @@ class Ar:
                 with self as open_file:
                     open_file.seek(member.offset)
                     return open_file.read(member.size)
+        return None
